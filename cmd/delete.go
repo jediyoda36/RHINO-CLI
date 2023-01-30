fabb50727f9d051b817521a9b34118e3f6440fbb
zhuhe <zhuhe_cs@foxmail.com>
2023-01-30 11:23:00 +0800

    add rhino delete

--- cmd/delete.go
+++ cmd/delete.go
@@ -1,40 +1,102 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
+	"os"
 	"fmt"
-
+	"context"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"path/filepath"
 	"github.com/spf13/cobra"
 )
 
-// deleteCmd represents the delete command
-var deleteCmd = &cobra.Command{
-	Use:   "delete",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+var namespace string
+var config string
+var name string
+var gvr = schema.GroupVersionResource{Group: "openrhino.org", Version: "v1alpha1", Resource: "rhinojobs"}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("delete called")
+type RhinoJobSpec struct {
+	Image string `json:"image"`
+	Parallelism *int32 `json:"parallelism,omitempty"`
+	TTL        *int32   `json:"ttl,omitempty"`
+	AppExec    string   `json:"appExec"`
+	AppArgs    []string `json:"appArgs,omitempty"`
+	DataServer string   `json:"dataServer,omitempty"`
+	DataPath   string   `json:"dataPath,omitempty"`
+}
+
+type RhinoJobStatus struct {
+	JobStatus JobStatus `json:"jobStatus"`
+}
+type JobStatus string
+
+const (
+	Pending   JobStatus = "Pending"
+	Running   JobStatus = "Running"
+	Failed    JobStatus = "Failed"
+	Completed JobStatus = "Completed"
+)
+
+type RhinoJob struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   RhinoJobSpec   `json:"spec,omitempty"`
+	Status RhinoJobStatus `json:"status,omitempty"`
+}
+
+type RhinoJobList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []RhinoJob `json:"items"`
+}
+
+
+var deleteCmd = &cobra.Command{
+	Use:   "delete [name]",
+	Short: "Delete rhino job and rhino function",
+	Long: "\nDelete rhino job and rhino function",
+	RunE: func(cmd *cobra.Command, args []string) error{
+		if len(args) == 0 {
+			return fmt.Errorf("[name] cannot be empty")
+		}
+		name = args[0]
+		var kubeconfig string
+		if home := homedir.HomeDir(); home != "" {
+			kubeconfig = filepath.Join(home, ".kube", "config")
+		} else {
+			if len(config) == 0 {
+				fmt.Println("Error: kubeconfig file not found, please use --config to specify the absolute path")
+				os.Exit(0)
+			}
+			kubeconfig = config
+		}
+
+		config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			return err
+		}
+	
+		dynamicClient, err := dynamic.NewForConfig(config)
+		if err != nil {
+			return err
+		}
+	
+		err = dynamicClient.Resource(gvr).Namespace(namespace).Delete(context.TODO(), name, metav1.DeleteOptions{})
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(0)
+		}
+		fmt.Println("RhinoJob " + name + " deleted")
+		return nil	
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(deleteCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// deleteCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// deleteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	deleteCmd.Flags().StringVarP(&namespace, "namespace", "n", "default", "namespace of the rhinojob")
+	deleteCmd.Flags().StringVar(&config, "config", "", "kubernetes config path")
 }
