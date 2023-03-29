4574cae0ecc9ff6a9efb0b025b30b1550c6e2210
LI-Mingyu <limingyu@ict.ac.cn>
2023-03-29 18:55:21 +0800

    Refactored code and decoupled `rhino` command from the underlying calling to docker

--- cmd/cmd_utils.go
+++ cmd/cmd_utils.go
@@ -1,7 +1,18 @@
 package cmd
 
 import (
+	"context"
 	"strings"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
+
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/tools/clientcmd"
@@ -47,3 +58,125 @@ func getFuncName(image string) string {
 	funcName := strings.Split(nameTag[len(nameTag)-1], ":")[0]
 	return funcName
 }
+
+
+
+type DockerHelper struct {
+	ctx context.Context
+	cli *client.Client
+}
+
+func NewDockerHelper() (*DockerHelper, error) {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return nil, err
+	}
+
+	return &DockerHelper{
+		ctx: ctx,
+		cli: cli,
+	}, nil
+}
+
+func (dh *DockerHelper) checkAndPullImage(image string) error {
+	_, _, err := dh.cli.ImageInspectWithRaw(dh.ctx, image)
+	if err != nil {
+		if client.IsErrNotFound(err) {
+			fmt.Printf("Image %s not found, pulling from Docker Hub...\n", image)
+			// Pull the image
+			out, err := dh.cli.ImagePull(dh.ctx, image, types.ImagePullOptions{})
+			if err != nil {
+				return err
+			}
+			defer out.Close()
+			// Copy the output to stdout
+			_, err = io.Copy(os.Stdout, out)
+			if err != nil && err != io.EOF {
+				return err
+			}
+		} else {
+			return err
+		}
+	}
+	return nil
+}
+
+func (dh *DockerHelper) createAndStartContainer(r *DockerRunOptions, args []string) (string, error) {
+	// Configure the container
+	entrypoint := []string{"mpirun", "-np", strconv.Itoa(r.parallel), "/app/mpi-func"}
+	containerConfig := &container.Config{
+		Image:      args[0],
+		Entrypoint: entrypoint,
+		Cmd:        args[1:],
+		Env: []string{
+			"OMPI_MCA_btl_base_warn_component_unused=0", // Suppress OpenMPI warning, if OpenMPI is used
+		},
+	}
+	hostConfig := &container.HostConfig{}
+
+	// Bind mount a volume if "-v" is specified
+	hostPath, containerPath := "", ""
+	if r.volume != "" {
+		volumeParts := strings.SplitN(r.volume, ":", 2)
+		if len(volumeParts) == 2 {
+			hostPath, containerPath = volumeParts[0], volumeParts[1]
+		} else {
+			return "", fmt.Errorf("invalid volume format, should be <host-path>:<container-path>")
+		}
+	}
+	if hostPath != "" && containerPath != "" {
+		bind := []string{hostPath + ":" + containerPath}
+		hostConfig.Binds = bind
+	}
+
+	// Create and start the container
+	resp, err := dh.cli.ContainerCreate(dh.ctx, containerConfig, hostConfig, nil, nil, "")
+	if err != nil {
+		return "", err
+	}
+	if err := dh.cli.ContainerStart(dh.ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		return "", err
+	}
+
+	return resp.ID, nil
+}
+
+func (dh *DockerHelper) getContainerLogs(containerID string) error {
+	logOptions := types.ContainerLogsOptions{
+		ShowStdout: true,
+		ShowStderr: true,
+		Follow:     true,
+	}
+	logReader, err := dh.cli.ContainerLogs(dh.ctx, containerID, logOptions)
+	if err != nil {
+		return err
+	}
+	defer logReader.Close()
+
+	// Use a demultiplexer to split stdout and stderr, and copy the container logs to the program output
+	_, err = stdcopy.StdCopy(os.Stdout, os.Stderr, logReader)
+	if err != nil && err != io.EOF {
+		return fmt.Errorf("error copying container logs: %v", err)
+	}
+
+	return nil
+}
+
+func (dh *DockerHelper) waitForContainerExit(containerID string) error {
+	waitCh, errCh := dh.cli.ContainerWait(dh.ctx, containerID, container.WaitConditionNotRunning)
+	var waitResp container.WaitResponse
+	var waitErr error
+
+	select {
+	case waitResp = <-waitCh:
+		if waitResp.StatusCode != 0 {
+			return fmt.Errorf("container exited with non-zero status: %d", waitResp.StatusCode)
+		}
+	case waitErr = <-errCh:
+		return waitErr
+	}
+
+	return nil
+}
+
--- cmd/dockerrun.go
+++ cmd/dockerrun.go
@@ -1,24 +1,15 @@
 package cmd
 
 import (
-	"context"
 	"fmt"
-	"io"
 	"os"
-	"strconv"
-	"strings"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/client"
-	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/spf13/cobra"
 )
 
 type DockerRunOptions struct {
 	parallel int
 	volume   string
-	funcName string
 }
 
 func NewDockerRunCommand() *cobra.Command {
@@ -45,106 +36,35 @@ func (r *DockerRunOptions) dockerRun(cmd *cobra.Command, args []string) error {
 		cmd.Help()
 		os.Exit(0)
 	}
-	r.funcName = getFuncName(args[0])
 	if r.parallel < 1 {
 		return fmt.Errorf("the number of MPI processes (--np) must be greater than 0")
 	}
-	// Handle -v option
-	hostPath, containerPath := "", ""
-	if r.volume != "" {
-		volumeParts := strings.SplitN(r.volume, ":", 2)
-		if len(volumeParts) == 2 {
-			hostPath, containerPath = volumeParts[0], volumeParts[1]
-		} else {
-			return fmt.Errorf("invalid volume format, should be <host-path>:<container-path>")
-		}
-	}
 
-	// Set up the context and Docker client
-	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+
+	// Create a DockerHelper instance
+	helper, err := NewDockerHelper()
 	if err != nil {
 		return err
 	}
 
-	// Check if the image exists
-	_, _, err = cli.ImageInspectWithRaw(ctx, args[0])
+	// Check if the image exists and pull it if necessary
+	err = helper.checkAndPullImage(args[0])
 	if err != nil {
-		if client.IsErrNotFound(err) {
-			fmt.Printf("Image %s not found, pulling from Docker Hub...\n", args[0])
-			// Pull the image
-			out, err := cli.ImagePull(ctx, args[0], types.ImagePullOptions{})
-			if err != nil {
-				return err
-			}
-			defer out.Close()
-			// Copy the output to stdout
-			_, err = io.Copy(os.Stdout, out)
-			if err != nil && err != io.EOF {
-				return err
-			}
-		} else {
-			return err
-		}
-	}
-
-	// Configure the container
-	entrypoint := []string{"mpirun", "-np", strconv.Itoa(r.parallel), "/app/mpi-func"}
-	containerConfig := &container.Config{
-		Image:      args[0],
-		Entrypoint: entrypoint,
-		Cmd:        args[1:],
-		Env: []string{
-			"OMPI_MCA_btl_base_warn_component_unused=0", // Suppress OpenMPI warning, if OpenMPI is used
-		},
-	}
-	hostConfig := &container.HostConfig{}
-	// Bind mount a volume if "-v" is specified
-	if hostPath != "" && containerPath != "" {
-		bind := []string{hostPath + ":" + containerPath}
-		hostConfig.Binds = bind
+		return err
 	}
 
 	// Create and start the container
-	resp, err := cli.ContainerCreate(ctx, containerConfig, hostConfig, nil, nil, "")
+	containerID, err := helper.createAndStartContainer(r, args)
 	if err != nil {
-		return err
-	}
-	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
 		return err
 	}
 
 	// Get the container logs
-	logOptions := types.ContainerLogsOptions{
-		ShowStdout: true,
-		ShowStderr: true,
-		Follow:     true,
-	}
-	logReader, err := cli.ContainerLogs(ctx, resp.ID, logOptions)
+	err = helper.getContainerLogs(containerID)
 	if err != nil {
 		return err
 	}
-	defer logReader.Close()
-
-	// Use a demultiplexer to split stdout and stderr, and copy the container logs to the program output
-	_, err = stdcopy.StdCopy(os.Stdout, os.Stderr, logReader)
-	if err != nil && err != io.EOF {
-		return fmt.Errorf("error copying container logs: %v", err)
-	}
 
 	// Wait for the container to exit and retrieve the exit status
-	waitCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
-	var waitResp container.WaitResponse
-	var waitErr error
-
-	select {
-	case waitResp = <-waitCh:
-		if waitResp.StatusCode != 0 {
-			return fmt.Errorf("container exited with non-zero status: %d", waitResp.StatusCode)
-		}
-	case waitErr = <-errCh:
-		return waitErr
-	}
-
-	return nil
+	return helper.waitForContainerExit(containerID)
 }
